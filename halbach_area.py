0d56d27d9f91d795550649f1e16df8553aebf07a
Ben Shepherd <bjashepherd@gmail.com>
2020-12-22 16:39:02 +0000

    Add more options

--- halbach_area.py
+++ halbach_area.py
@@ -3,15 +3,20 @@ import numpy as np
 import random
 from PyQt5 import QtWidgets
 from PyQt5.QtGui import QIcon
-from PyQt5.QtCore import pyqtSlot
+from PyQt5 import QtCore
+from functools import partial
+from time import sleep
 import matplotlib
+
 matplotlib.use('Qt5Agg')
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg, NavigationToolbar2QT as NavigationToolbar
 from matplotlib.figure import Figure
 from matplotlib.patches import Polygon
 from matplotlib.collections import PatchCollection
 import subprocess
+
 halbach_exe = r"HalbachArea.exe"
+
 
 def execute(cmd):
     popen = subprocess.Popen(cmd, stdout=subprocess.PIPE, universal_newlines=True)
@@ -31,7 +36,6 @@ class MplCanvas(FigureCanvasQTAgg):
         super(MplCanvas, self).__init__(fig)
 
 
-
 class App(QtWidgets.QWidget):
 
     def __init__(self):
@@ -40,74 +44,125 @@ class App(QtWidgets.QWidget):
         self.layout = self.controls = self.plot = None
         self.initUI()
 
-    def appendControl(self, name, default, switch, units=None, checked=None, digits=2):
+    def appendControl(self, name, default, switch, units=None, checked=None, digits=2, step=1):
         """Add a control.
-        Set checked to True or False for a checkbox, otherwise will be a label."""
+        Set checked to True or False for a checkbox, otherwise will be a label.
+        Set step to a list to make a dropdown."""
         hbox = QtWidgets.QHBoxLayout()
+        if step is None:
+            spinbox = None
+        elif isinstance(step, list):
+            spinbox = QtWidgets.QComboBox()
+            [spinbox.addItem(i) for i in step]
+            spinbox.setCurrentIndex(1)
+        else:
+            spinbox = QtWidgets.QDoubleSpinBox(decimals=digits) if digits > 0 else QtWidgets.QSpinBox()
+            spinbox.setMaximum(2147483647)
+            spinbox.setMinimum(0)
+            spinbox.setSingleStep(step)
+            spinbox.setSuffix(f' {units}' if units else '')
+            spinbox.setValue(default)
         if checked is None:
             checkbox = QtWidgets.QLabel(name)
         else:
             checkbox = QtWidgets.QCheckBox(name)
             checkbox.setChecked(checked)
+            if spinbox is not None:
+                spinbox.valueChanged.connect(partial(self.spinbox_changed, checkbox))
+                checkbox.clicked.connect(self.checkbox_changed)
         hbox.addWidget(checkbox)
-        spinbox = QtWidgets.QDoubleSpinBox(decimals=digits) if digits > 0 else QtWidgets.QSpinBox()
-        spinbox.setMaximum(2147483647)
-        spinbox.setMinimum(-2147483648)
-        spinbox.setSuffix(f' {units}' if units else '')
-        spinbox.setValue(default)
         hbox.addWidget(spinbox)
         self.layout.addLayout(hbox)
         self.controls.append((switch, spinbox, checkbox if checked in (True, False) else None))
+        return spinbox
 
     def initUI(self):
         self.setWindowTitle(self.title)
         # self.setGeometry(self.left, self.top, self.width, self.height)
 
+        vert_layout = QtWidgets.QVBoxLayout()
         horiz_layout = QtWidgets.QHBoxLayout()
+        vert_layout.addLayout(horiz_layout)
         self.layout = QtWidgets.QVBoxLayout()
         horiz_layout.addLayout(self.layout)
         self.controls = []
 
         self.appendControl('Radius', 5, 'R', 'mm', digits=1)
-        self.appendControl('Dipole', 0.2, 'dipole', 'T', False)
-        self.appendControl('Quadrupole', 300, 'quad', 'T/m', True)
+        self.appendControl('Good field region', 3, 'gfr', 'mm', False, digits=1)
+        self.appendControl('Dipole', 0.2, 'dipole', 'T', False, step=0.1)
+        self.appendControl('Quadrupole', 300, 'quad', 'T/m', True, step=10)
         self.appendControl('Sextupole', 66, 'sext', 'T/m²', False)
-        self.appendControl('Octupole', 888, 'oct', 'T/m³', False)
-        self.appendControl('Remanent field', 1.07, 'Br', 'T')
-        self.appendControl('Wedges', 16, 'wedges', digits=0)
-        self.appendControl('Symmetry', 1, 'symmetry', digits=0)
+        self.appendControl('Octupole', 888, 'oct', 'T/m³', False, step=10)
+        self.appendControl('Remanent field', 1.07, 'Br', 'T', step=0.1)
+        self.appendControl('Wedges', 16, 'wedges', digits=0, step=4)
+        self.appendControl('Symmetry', '2', 'symmetry', step=['None', 'Top/bottom', 'Quad'])
+        self.appendControl('Offset by half-width', False, 'halfoff', checked=False, step=None)
+        self.appendControl('Midplane half-height', 0, 'ymidplane', 'mm', digits=2)
+        self.appendControl('Remove midplanes', '2', 'midplanes', step=['Left only', 'Horizontal', 'Cross'])
+        self.appendControl('Remove wedges by midplane', False, 'removeadjacent', checked=False, step=None)
 
         hbox = QtWidgets.QHBoxLayout()
         self.checkbox = QtWidgets.QCheckBox('Auto')
         # self.checkbox.clicked.connect(self.run_simulation)
         hbox.addWidget(self.checkbox)
-        run_button = QtWidgets.QPushButton('Run')
-        run_button.clicked.connect(self.run_simulation)
-        hbox.addWidget(run_button)
+        self.run_button = QtWidgets.QPushButton('Run')
+        self.run_button.clicked.connect(self.run_simulation)
+        hbox.addWidget(self.run_button)
         self.layout.addLayout(hbox)
+        self.status_bar = QtWidgets.QLabel()
+        vert_layout.addWidget(self.status_bar)
 
         self.plot = MplCanvas(self, width=5, height=4, dpi=100)
         toolbar = NavigationToolbar(self.plot, self)  # TODO: make it appear above the plot
         horiz_layout.addWidget(self.plot)
 
-        self.setLayout(horiz_layout)
+        self.setLayout(vert_layout)
         self.show()
 
-    @pyqtSlot()
+    def spinbox_changed(self, checkbox):
+        checkbox.setChecked(True)
+
+    def checkbox_changed(self):
+        n_checked = 0
+        for switch, spinbox, checkbox in self.controls:
+            if checkbox is not None and checkbox.text().endswith('pole') and checkbox.isChecked():
+                n_checked += 1
+        if n_checked == 0:
+            self.status_bar.setText('Select at least one multipole.')
+            self.run_button.setEnabled(False)
+        else:
+            self.status_bar.setText('Ready.')
+            self.run_button.setEnabled(True)
+
+    @QtCore.pyqtSlot()
     def run_simulation(self):
         cmd = [halbach_exe, ]
         for switch, spinbox, checkbox in self.controls:
             if checkbox is None or checkbox.isChecked():
-                value = spinbox.value() * (0.001 if switch == 'R' else 1)
+                if spinbox is None:
+                    value = 1
+                elif isinstance(spinbox, QtWidgets.QComboBox):
+                    value = (1, 2, 4)[spinbox.currentIndex()]
+                else:
+                    value = spinbox.value() * (0.001 if spinbox.suffix() == ' mm' else 1)
                 cmd.append(f'{switch}={value}')
 
         # if self.checkbox.isChecked():
-        for line in execute(cmd):
-            print(line, end="")
-            # process = subprocess.Popen(cmd, stdout=subprocess.PIPE)
-            # output, err = process.communicate()
-            # exit_code = process.wait()
-            # print(output)
+        self.status_bar.setText('Running...')
+        self.run_button.setEnabled(False)
+        self.status_bar.update()
+        print(cmd)
+        popen = subprocess.Popen(cmd, stdout=subprocess.PIPE, universal_newlines=True)
+        while popen.poll() is None:
+            sleep(0.5)
+        stdout, stderr = popen.communicate()
+        self.run_button.setEnabled(True)
+        print(stdout)
+        if popen.returncode:
+            self.status_bar.setText(stdout.splitlines()[-1])
+            return
+        self.status_bar.setText('Done.')
+
         polygons = np.loadtxt(r"magnet.csv", skiprows=1, usecols=range(1, 11), delimiter=',')
         min_width = min(np.ptp(polygons[:, 2::2], axis=1))
         min_height = min(np.ptp(polygons[:, 3::2], axis=1))
@@ -118,13 +173,9 @@ class App(QtWidgets.QWidget):
         shapes = [Polygon(polygon[2:10].reshape(4, 2)) for polygon in polygons]
         self.plot.axes.add_collection(PatchCollection(shapes))
         for polygon in polygons:
-        #     print(polygon[[2, 4, 6, 8, 2]], polygon[[3, 5, 7, 9, 3]])
-        #     self.plot.axes.plot(polygon[[2, 4, 6, 8, 2]], polygon[[3, 5, 7, 9, 3]], 'r')
-            bx = polygon[0] * scale
-            by = polygon[1] * scale
+            bx, by = polygon[:2] * scale
             self.plot.axes.arrow(np.mean(polygon[2::2]) - bx / 2, np.mean(polygon[3::2]) - by / 2,
-                             bx, by, width=0.1*np.sqrt(bx**2 + by**2), length_includes_head=True)
-            # self.plot.axes.plot(range(4), random.sample(range(10), 4))
+                                 bx, by, width=0.1 * np.sqrt(bx ** 2 + by ** 2), length_includes_head=True)
         self.plot.axes.set_xlim([min(polygons[:, 2::2].flat), max(polygons[:, 2::2].flat)])
         self.plot.axes.set_ylim([min(polygons[:, 3::2].flat), max(polygons[:, 3::2].flat)])
         self.plot.draw()
