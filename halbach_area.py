e0cd002de6604c206cf2fb0fb0f5cb939037f305
Ben Shepherd <bjashepherd@gmail.com>
2020-12-23 16:03:28 +0000

    Fix for buffering issue Comments Toolbar at top

--- halbach_area.py
+++ halbach_area.py
@@ -1,11 +1,10 @@
 import sys
 import numpy as np
-import random
-from PyQt5 import QtWidgets
-from PyQt5.QtGui import QIcon
-from PyQt5 import QtCore
+import os
+import re
+import subprocess
+from PyQt5 import QtWidgets, QtCore
 from functools import partial
-from time import sleep
 import matplotlib
 
 matplotlib.use('Qt5Agg')
@@ -13,19 +12,8 @@ from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg, NavigationTool
 from matplotlib.figure import Figure
 from matplotlib.patches import Polygon
 from matplotlib.collections import PatchCollection
-import subprocess
 
 halbach_exe = r"HalbachArea.exe"
-
-
-def execute(cmd):
-    popen = subprocess.Popen(cmd, stdout=subprocess.PIPE, universal_newlines=True)
-    for stdout_line in iter(popen.stdout.readline, ""):
-        yield stdout_line
-    popen.stdout.close()
-    return_code = popen.wait()
-    if return_code:
-        raise subprocess.CalledProcessError(return_code, cmd)
 
 
 class MplCanvas(FigureCanvasQTAgg):
@@ -41,42 +29,48 @@ class App(QtWidgets.QWidget):
     def __init__(self):
         super().__init__()
         self.title = 'HalbachArea'
-        self.layout = self.controls = self.plot = None
-        self.initUI()
+        self.auto_checkbox = self.mid_wedges = self.mid_symmetry = self.layout = self.controls = self.plot = None
+        self.status_bar = self.run_button = self.progress_bar = None
+        self.init_ui()
 
     def appendControl(self, name, default, switch, units=None, checked=None, digits=2, step=1):
         """Add a control.
         Set checked to True or False for a checkbox, otherwise will be a label.
         Set step to a list to make a dropdown."""
         hbox = QtWidgets.QHBoxLayout()
-        if step is None:
-            spinbox = None
-        elif isinstance(step, list):
-            spinbox = QtWidgets.QComboBox()
-            [spinbox.addItem(i) for i in step]
-            spinbox.setCurrentIndex(1)
+        if step is None:  # checkbox only
+            control = None
+        elif isinstance(step, list):  # dropdown box
+            control = QtWidgets.QComboBox()
+            [control.addItem(i) for i in step]
+            control.setCurrentIndex(1)
+            change_event = control.currentIndexChanged
         else:
-            spinbox = QtWidgets.QDoubleSpinBox(decimals=digits) if digits > 0 else QtWidgets.QSpinBox()
-            spinbox.setMaximum(2147483647)
-            spinbox.setMinimum(0)
-            spinbox.setSingleStep(step)
-            spinbox.setSuffix(f' {units}' if units else '')
-            spinbox.setValue(default)
-        if checked is None:
+            control = QtWidgets.QDoubleSpinBox(decimals=digits) if digits > 0 else QtWidgets.QSpinBox()
+            control.setMaximum(2147483647)
+            control.setMinimum(0)
+            control.setSingleStep(step)
+            control.setSuffix(f' {units}' if units else '')
+            control.setValue(default)
+            change_event = control.valueChanged
+        if checked is None:  # non-optional control
             checkbox = QtWidgets.QLabel(name)
-        else:
+            change_event.connect(self.autorun)
+        else:  # optional so include a checkbox
             checkbox = QtWidgets.QCheckBox(name)
             checkbox.setChecked(checked)
-            if spinbox is not None:
-                spinbox.valueChanged.connect(partial(self.spinbox_changed, checkbox))
+            if control is not None:
+                control.valueChanged.connect(partial(self.spinbox_changed, checkbox))
                 checkbox.clicked.connect(self.checkbox_changed)
+            else:
+                checkbox.clicked.connect(self.autorun)
         hbox.addWidget(checkbox)
-        hbox.addWidget(spinbox)
+        hbox.addWidget(control)
         self.layout.addLayout(hbox)
-        self.controls.append((switch, spinbox, checkbox if checked in (True, False) else None))
-        return spinbox
+        self.controls.append((switch, control, checkbox if checked in (True, False) else None))
+        return checkbox if control is None else control
 
-    def initUI(self):
+    def init_ui(self):
         self.setWindowTitle(self.title)
         # self.setGeometry(self.left, self.top, self.width, self.height)
 
@@ -91,38 +85,60 @@ class App(QtWidgets.QWidget):
         self.appendControl('Good field region', 3, 'gfr', 'mm', False, digits=1)
         self.appendControl('Dipole', 0.2, 'dipole', 'T', False, step=0.1)
         self.appendControl('Quadrupole', 300, 'quad', 'T/m', True, step=10)
-        self.appendControl('Sextupole', 66, 'sext', 'T/m²', False)
+        self.appendControl('Sextupole', 66, 'sext', 'T/m²', False, step=10)
         self.appendControl('Octupole', 888, 'oct', 'T/m³', False, step=10)
         self.appendControl('Remanent field', 1.07, 'Br', 'T', step=0.1)
         self.appendControl('Wedges', 16, 'wedges', digits=0, step=4)
         self.appendControl('Symmetry', '2', 'symmetry', step=['None', 'Top/bottom', 'Quad'])
         self.appendControl('Offset by half-width', False, 'halfoff', checked=False, step=None)
-        self.appendControl('Midplane half-height', 0, 'ymidplane', 'mm', digits=2)
-        self.appendControl('Remove midplanes', '2', 'midplanes', step=['Left only', 'Horizontal', 'Cross'])
-        self.appendControl('Remove wedges by midplane', False, 'removeadjacent', checked=False, step=None)
+        mhh_dropdown = self.appendControl('Midplane half-height', 0, 'ymidplane', 'mm', digits=2)
+        mhh_dropdown.valueChanged.connect(self.check_midplane_controls)
+        self.mid_symmetry = self.appendControl('Remove midplanes', '2', 'midplanes',
+                                               step=['Left only', 'Horizontal', 'Cross'])
+        self.mid_wedges = self.appendControl('Remove wedges by midplane', False, 'removeadjacent', checked=False,
+                                             step=None)
+        self.check_midplane_controls(0)
 
         hbox = QtWidgets.QHBoxLayout()
-        self.checkbox = QtWidgets.QCheckBox('Auto')
-        # self.checkbox.clicked.connect(self.run_simulation)
-        hbox.addWidget(self.checkbox)
+        self.auto_checkbox = QtWidgets.QCheckBox('Auto')
+        self.auto_checkbox.setChecked(True)
+        hbox.addWidget(self.auto_checkbox)
         self.run_button = QtWidgets.QPushButton('Run')
         self.run_button.clicked.connect(self.run_simulation)
         hbox.addWidget(self.run_button)
         self.layout.addLayout(hbox)
+        self.progress_bar = QtWidgets.QProgressBar()
+        self.progress_bar.setMaximum(25)
+        self.progress_bar.setValue(0)
+        self.layout.addWidget(self.progress_bar)
         self.status_bar = QtWidgets.QLabel()
         vert_layout.addWidget(self.status_bar)
 
         self.plot = MplCanvas(self, width=5, height=4, dpi=100)
-        toolbar = NavigationToolbar(self.plot, self)  # TODO: make it appear above the plot
+        NavigationToolbar(self.plot, self.plot)
         horiz_layout.addWidget(self.plot)
 
         self.setLayout(vert_layout)
         self.show()
+        self.autorun()
+
+    def check_midplane_controls(self, value):
+        """Grey out midplane controls when midplane gap is set to zero."""
+        self.mid_wedges.setEnabled(value > 0)
+        self.mid_symmetry.setEnabled(value > 0)
 
     def spinbox_changed(self, checkbox):
+        """Check the relevant box when one of the spinboxes is changed."""
         checkbox.setChecked(True)
+        self.autorun()
+
+    def autorun(self):
+        """Run a new simulation when something changes."""
+        if self.auto_checkbox.isChecked() and self.run_button.isEnabled():
+            self.run_simulation()
 
     def checkbox_changed(self):
+        """Check that at least one multipole is checked."""
         n_checked = 0
         for switch, spinbox, checkbox in self.controls:
             if checkbox is not None and checkbox.text().endswith('pole') and checkbox.isChecked():
@@ -133,45 +149,69 @@ class App(QtWidgets.QWidget):
         else:
             self.status_bar.setText('Ready.')
             self.run_button.setEnabled(True)
+            self.autorun()
 
-    @QtCore.pyqtSlot()
     def run_simulation(self):
-        cmd = [halbach_exe, ]
-        for switch, spinbox, checkbox in self.controls:
-            if checkbox is None or checkbox.isChecked():
-                if spinbox is None:
-                    value = 1
-                elif isinstance(spinbox, QtWidgets.QComboBox):
-                    value = (1, 2, 4)[spinbox.currentIndex()]
-                else:
-                    value = spinbox.value() * (0.001 if spinbox.suffix() == ' mm' else 1)
-                cmd.append(f'{switch}={value}')
+        """Run the simulation using specified parameters."""
+        # For some reason the output from halbach_exe is buffered so we can't read it until the program completes.
+        # I found a workaround but it requires the use of winpty: https://github.com/rprichard/winpty/releases
+        # If winpty is not available it will just use halbach_exe and wait until the program exits.
+        # https://stackoverflow.com/questions/11516258/what-is-the-equivalent-of-unbuffer-program-on-windows
+        winpty = 'winpty.exe'
+        cmd = [winpty, '-Xallow-non-tty', '-Xplain', halbach_exe] if os.path.exists(winpty) else [halbach_exe, ]
 
-        # if self.checkbox.isChecked():
-        self.status_bar.setText('Running...')
-        self.run_button.setEnabled(False)
-        self.status_bar.update()
+        # add command-line arguments
+        for switch, control, checkbox in self.controls:
+            if checkbox is None or checkbox.isChecked():
+                if control is None:  # just a checkbox
+                    value = 1
+                elif isinstance(control, QtWidgets.QComboBox):  # dropdown, options are always 1, 2, 4
+                    value = (1, 2, 4)[control.currentIndex()]
+                else:  # spinbox
+                    value = control.value() * (0.001 if control.suffix() == ' mm' else 1)
+                cmd.append(f'{switch}={value}')
+                if switch == 'ymidplane' and value == 0:
+                    break  # don't bother with the last two
         print(cmd)
-        popen = subprocess.Popen(cmd, stdout=subprocess.PIPE, universal_newlines=True)
-        while popen.poll() is None:
-            sleep(0.5)
-        stdout, stderr = popen.communicate()
+
+        self.status_bar.setText('Running...')
+        self.progress_bar.setValue(0)
+        self.run_button.setEnabled(False)
+        QtCore.QCoreApplication.processEvents()
+        process = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
+        status_line = re.compile('Iteration (\d+)')
+        failed = False
+        while process.poll() is None:
+            stdout = process.stdout.readline()
+            try:
+                self.progress_bar.setValue(int(status_line.match(stdout).group(1)))
+            except AttributeError:  # not a line with "Iteration X" in it
+                pass
+            self.status_bar.setText(stdout)
+            print(stdout, end='')
+            failed |= 'ERROR' in stdout
+            QtCore.QCoreApplication.processEvents()  # update the GUI
+        print('')
         self.run_button.setEnabled(True)
-        print(stdout)
-        if popen.returncode:
-            self.status_bar.setText(stdout.splitlines()[-1])
+        if failed:
+            print(process.returncode)
+            self.status_bar.setText('Failed.')
+            self.progress_bar.setValue(0)
             return
         self.status_bar.setText('Done.')
+        self.progress_bar.setValue(self.progress_bar.maximum())
 
-        polygons = np.loadtxt(r"magnet.csv", skiprows=1, usecols=range(1, 11), delimiter=',')
+        # get the magnet shapes from the output CSV file
+        # format is Bx, By, x0, y0, x1, y1, x2, y2, x3, y3
+        polygons = np.loadtxt(r"magnet.csv", skiprows=1, usecols=range(1, 11), delimiter=',') * 1000
         min_width = min(np.ptp(polygons[:, 2::2], axis=1))
         min_height = min(np.ptp(polygons[:, 3::2], axis=1))
         max_b = max(polygons[:, :2].flat)
-        scale = min(min_width, min_height) / max_b
-        print(scale)
-        self.plot.axes.cla()
+        scale = min(min_width, min_height) / max_b  # how to represent Bx/By vectors on graph?
+        self.plot.axes.cla()  # clear axes
         shapes = [Polygon(polygon[2:10].reshape(4, 2)) for polygon in polygons]
         self.plot.axes.add_collection(PatchCollection(shapes))
+        # add remanent field vectors
         for polygon in polygons:
             bx, by = polygon[:2] * scale
             self.plot.axes.arrow(np.mean(polygon[2::2]) - bx / 2, np.mean(polygon[3::2]) - by / 2,
